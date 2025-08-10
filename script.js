88e8cf7c5eeddadcb1e6acec77f4f8860861520d
aquaseals <itzme.haya@gmail.com>
2025-08-10 12:08:56 -0400

    finished suck assets

--- script.js
+++ script.js
@@ -1,80 +1,98 @@
+let playerSpeed = 2
+let river
+
 function startGame() {
     myGameArea.start();
 
-    duck1 = new duck(50, 40, ["/assets/frame1.PNG", "/assets/frame2.PNG"], 50, 60)
-    duck1.walk()
+    duck1 = new component(50, 40, ["/assets/frame1.PNG", "/assets/frame2.PNG"], 50, 60);
+    duck1.walk();
+
+    river = new component(1200, 500, ["/assets/river.png", "/assets/river.png"], 0, 500)
+
 }
 
 var myGameArea = {
     canvas : document.getElementById("map"),
     start : function() {
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20)
+        this.interval = setInterval(updateGameArea, 20);
     },
     clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
-}
+};
 
-function duck(width, height, frames, x, y) {
-    this.width = width
-    this.height = height
-    this.x = x
-    this.y = y
+// function obstacle(width, height, x, y, newSpeed, color) {
+//     this.width = width
+//     this.height = height
+//     this.x = x
+//     this.y = y
+//     this.color = color
 
-    this.image = new Image()
-    this.image.src = frames[0]
+//     ctx = myGameArea.context;
+//     ctx.fillStyle = color;
+//     ctx.fillRect(this.x, this.y, this.width, this.height);
+
+//     this.update = function() {
+//         ctx = myGameArea.context;
+//         ctx.fillStyle = this.color;
+//         ctx.fillRect(this.x, this.y, this.width, this.height);
+//     };
+// }
+
+function component(width, height, frames, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.frames = frames
+
+    this.image = new Image();
+    this.image.src = this.frames[0];
+    this.image.onload = () => { // Ensure image is loaded before drawing
+        this.update();
+    };
     this.update = function() {
-        ctx = myGameArea.context
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
-    }
+        let ctx = myGameArea.context;
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    };
     this.walk = function() {
-        let i = 0
-        let animate = setInterval(() => {
-            if (i >= frames.length) {
-                i = 0
+        let i = 0;
+        setInterval(() => {
+            if (i >= this.frames.length) {
+                i = 0;
             }
-            this.image.src = frames[i]
-            i++
-        }, 100)
-    }
+            this.image.src = this.frames[i];
+            i++;
+        }, 100);
+    };
 }
 
-function updateGameArea()  {
-    myGameArea.clear()
-    //duck1.x += 1
-    duck1.update()
+// Track pressed keys
+let keys = {};
+
+document.addEventListener("keydown", function(e) {
+    keys[e.key] = true;
+});
+
+document.addEventListener("keyup", function(e) {
+    keys[e.key] = false;
+});
+
+function updateGameArea() {
+    myGameArea.clear();
+
+    river.update()
+
+    // Movement
+    if (keys["ArrowUp"]) duck1.y -= playerSpeed;
+    if (keys["ArrowDown"]) duck1.y += playerSpeed;
+    if (keys["ArrowLeft"]) duck1.x -= playerSpeed;
+    if (keys["ArrowRight"]) duck1.x += playerSpeed;
+
+    duck1.update();
 }
 
-function moveup() {
-  duck1.y -= 1;
-}
 
-function movedown() {
-  duck1.y += 1;
-}
 
-function moveleft() {
-  duck1.x -= 1;
-}
 
-function moveright() {
-  duck1.x += 1;
-}
-
-document.addEventListener('keydown', function(e) {
-    let key = e.key
-    if (key === 'ArrowUp') {
-        moveup()
-
-    } else if (key === 'ArrowDown') {
-        movedown()
-
-    } else if (key === 'ArrowLeft') {
-        moveleft()
-
-    } else if (key === 'ArrowRight') {
-        moveright()
-    }
-})
